dff395cf2e3693d9cee680bb5839390756ea24cd
shadownessguy@gmail.com <shadownessguy@gmail.com>
2013-12-01 18:05:06 +0800

    components page

---
+++ Canvas/CSComponentsViewController.h
@@ -0,0 +1,13 @@
+//
+//  CSComponentsViewController.h
+//  Canvas
+//
+//  Created by Meng To on 1/12/13.
+//  Copyright (c) 2013 Canvas. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface CSComponentsViewController : UIViewController
+
+@end
---
+++ Canvas/CSComponentsViewController.m
@@ -0,0 +1,42 @@
+//
+//  CSComponentsViewController.m
+//  Canvas
+//
+//  Created by Meng To on 1/12/13.
+//  Copyright (c) 2013 Canvas. All rights reserved.
+//
+
+#import "CSComponentsViewController.h"
+
+@interface CSComponentsViewController ()
+
+@end
+
+@implementation CSComponentsViewController
+
+- (id)initWithNibName:(NSString *)nibNameOrNil bundle:(NSBundle *)nibBundleOrNil
+{
+    self = [super initWithNibName:nibNameOrNil bundle:nibBundleOrNil];
+    if (self) {
+        // Custom initialization
+    }
+    return self;
+}
+
+- (void)viewDidLoad
+{
+    [super viewDidLoad];
+	[self setNeedsStatusBarAppearanceUpdate];
+}
+
+-(UIStatusBarStyle)preferredStatusBarStyle{
+    return UIStatusBarStyleLightContent;
+}
+
+- (void)didReceiveMemoryWarning
+{
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+@end
